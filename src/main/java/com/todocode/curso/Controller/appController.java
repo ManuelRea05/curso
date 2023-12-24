a5aa258f64e9533777e3200d006f852185049689
ManuelRea05 <agustinrea105@gmail.com>
2023-12-23 21:35:36 -0300

    asdasd

--- src/main/java/com/todocode/curso/Controller/appController.java
+++ src/main/java/com/todocode/curso/Controller/appController.java
@@ -1,16 +1,33 @@
 package com.todocode.curso.Controller;
 import com.todocode.curso.models.Cliente;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class appController {
-    @PostMapping("/cliente/")
+    /*@PostMapping("/cliente")
     public void crearCliente(@RequestBody Cliente cliente){
         System.out.println("Cliente creado");
         System.out.println("Nombre: "+cliente.getNombre()+
-                "Apellido: "+ cliente.getAp());
+                " Apellido: "+ cliente.getAp());
+    }*/
+    @GetMapping("/cliente/traer")
+    @ResponseBody
+    public List<Cliente> traerClientes() {
+        List<Cliente> listaClientes = new ArrayList<Cliente>();
+        listaClientes.add(new Cliente(1, "Zlatan", "Ibrahimovic"));
+        listaClientes.add(new Cliente(2, "Cristiano", "Ronaldo"));
+        listaClientes.add(new Cliente(3, "Liones", "Pessi"));
+        return listaClientes;
+    }
+
+    @GetMapping("/pruebaresponsive")
+    ResponseEntity<String> traerRespuesta(){
+
+        return new ResponseEntity<>("Esta es una prueba");
     }
 
 }
--- src/main/java/com/todocode/curso/models/Cliente.java
+++ src/main/java/com/todocode/curso/models/Cliente.java
@@ -7,4 +7,12 @@ public class Cliente {
     private String nombre;
     private String ap;
 
+    public Cliente() {
+    }
+
+    public Cliente(int id, String nombre, String ap) {
+        this.id = id;
+        this.nombre = nombre;
+        this.ap = ap;
+    }
 }
